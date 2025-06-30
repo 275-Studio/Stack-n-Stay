c063caacdaa6dd50f9b5de0d3859a57adb6df132
bung_anb <bunganabila1005@gmail.com>
2025-06-30 23:25:53 +0700

    fix : UI layout, level

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -12,29 +12,12 @@ public class BGMManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-
         Instance = this;
         DontDestroyOnLoad(gameObject);
-
         audioSource = GetComponent<AudioSource>();
         if (audioSource != null && !audioSource.isPlaying)
         {
             audioSource.loop = true;
-            audioSource.Play();
-        }
-    }
-
-    public void SetVolume(float volume)
-    {
-        if (audioSource != null)
-            audioSource.volume = volume;
-    }
-
-    public void ChangeClip(AudioClip clip)
-    {
-        if (audioSource != null && clip != null)
-        {
-            audioSource.clip = clip;
             audioSource.Play();
         }
     }
